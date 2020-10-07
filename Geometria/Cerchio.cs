3c6344ab77acb69a2509e58b631f329e07979cf1
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-10-07 12:02:53 +0200

    Add project files.

---
+++ Geometria/Cerchio.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    public class Cerchio
+    {
+        public const double PI = 3.14;
+        private double raggio;
+
+        public Cerchio(double r)
+        {
+            r = raggio;
+        }
+        public double CalcolaCirconferenza()
+        {
+            return 2 * PI * raggio;
+        }
+        public double CalcolaArea()
+        {
+            return PI*raggio * raggio;
+        }
+        public void CambiaRaggio(double nuovoRaggio)
+        {
+            if (nuovoRaggio > 0)
+                raggio = nuovoRaggio;
+            throw new Exception("Raggio non valido");
+
+        }
+        public double DammiRaggio()
+        {
+            return raggio;
+        }
+        public string StampaStringa()
+        {
+            return $"Cerchio(r={raggio},"+$"circonferenza={CalcolaCirconferenza()},"+$"area={CalcolaArea()})";
+        }
+    }
+}
---
+++ Geometria/Rettangolo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+
+    public class Rettangolo
+    {
+        private double x;
+        private double y;
+        private double width;
+        private double height;
+
+        public Rettangolo(double x, double y, double w, double h)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = w;
+            this.height = h;
+        }
+        public double CalcolaPerimetro()
+        {
+            return (width * 2) + (height * 2);
+        }
+        public double CalcolaArea()
+        {
+            return width * height;
+        }
+        public bool Contiene(Rettangolo r)
+        {
+            if (r.x >= this.x && r.width + r.x <= width + x)
+            {
+                if (r.y >= this.y && r.height + r.y <= height + y)
+                    return true;
+                return false;
+            }
+            else
+                return false;
+        }
+
+    }
+
+}
